890f20fe2988cd9db63cc2f7fd384aac8844d8b2
KnIfER <302772670@qq.com>
2017-11-11 14:22:39 +0800

    nothing to say..

--- libraryProjs/blurView/gen/eightbitlab/com/blurview/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package eightbitlab.com.blurview;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- libraryProjs/filepicker/gen/com/github/angads25/filepicker/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.github.angads25.filepicker;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
